d3717b10700d1f04a7165bbae2d46619e98714d5
Paulo Vinicius B Silva <vinicius@virtex.com.br>
2025-10-17 13:14:15 -0300

    Primeira versão do sistema

---
+++ app/service-worker.js
@@ -0,0 +1,64 @@
+const CACHE_NAME = 'relatorios-v1.3.0';
+const urlsToCache = [
+  '/app/',
+  '/app/index.html',
+  '/app/manifest.json',
+  '/relatorios.json',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://cdn.tailwindcss.com'
+];
+
+// Instalação - cacheia recursos
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+// Ativação - limpa caches antigos
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch - serve do cache ou busca online
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Retorna do cache se existir
+        if (response) {
+          return response;
+        }
+        
+        // Busca online e atualiza cache
+        return fetch(event.request).then(response => {
+          // Só cacheia respostas válidas
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+          
+          return response;
+        });
+      })
+  );
+});
